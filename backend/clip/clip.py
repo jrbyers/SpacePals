36383eaa587c248d6054a13dba01c47478b90a2c
jrbyers <john_byers@brown.edu>
2024-02-03 15:02:50 -0500

    clip test working

--- backend/clip/clip.py
+++ backend/clip/clip.py
@@ -1,64 +1,22 @@
-import clip
-import os
-import skimage
-import IPython.display
-import matplotlib.pyplot as plt
+from PIL import Image
+import requests
 import numpy as np
-from torch.utils.data import DataLoader
-from collections import OrderedDict
-import torch
-import torchvision as tv
-from torchvision.datasets import CIFAR10
 
-# RN50 will be fastest, while ViT-L will be the most performant.
-clip.available_models()
+from transformers import CLIPProcessor, CLIPModel
 
-model, preprocess = clip.load("RN50")   # this is the original preprocess
-model.cuda().eval()
-input_resolution = model.visual.input_resolution
-context_length = model.context_length
-vocab_size = model.vocab_size
-
-print("Model parameters:", f"{np.sum([int(np.prod(p.shape)) for p in model.parameters()]):,}")
-print("Input resolution:", input_resolution)
-print("Context length:", context_length)
-print("Vocab size:", vocab_size)
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 
-# generate 500 random CIFAR10 validation images with DataLoader
-#dataloader = DataLoader(dataset=cifar10_test, batch_size=500, shuffle=True)
-#images, labels = next(iter(dataloader))
+#url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+#image = Image.open(requests.get(url, stream=True).raw)
+image1 = Image.open("airplane.jpg")
+image = np.asarray(image1)
+
+inputs = processor(text=["cat", "dog", "airplane"], images=image, return_tensors="pt", padding=True)
 
 
-classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-label_strings = {0:'airplane', 1: 'automobile', 2:'bird', 3:'cat', 4:'deer',
-                 5:'dog', 6:'frog', 7:'horse', 8:'ship', 9:'truck'}
-
-# Setup text features
-text_descriptions = [f"This is a photo of a {label}" for label in classes]
-text_tokens = clip.tokenize(text_descriptions).cuda()
-
-# setup image featrues
-image_input = torch.tensor(np.stack(images)).cuda()
-with torch.no_grad():
-    image_features = model.encode_image(image_input).float()
-    text_features = model.encode_text(text_tokens).float()
-
-# generate probabilities for labels on every image
-text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-top_probs, top_labels = text_probs.cpu().topk(5, dim=-1)
-
-# count number of correct labels
-top_labels = np.array(top_labels)
-num_correct = 0
-for i in range(len(images)):
-  correct_label = labels[i].item()
-  clip_predic_label = top_labels[i][0]
-  if correct_label == clip_predic_label:
-    num_correct += 1
-
-# print out results
-percent_correct = num_correct/len(images) * 100
-print("Total number of images: " + str(len(images)))
-print("Number of correctly classified images: " + str(num_correct))
-print("Accuracy: " + str(percent_correct) + "%")
+outputs = model(**inputs)
+logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
+probs = logits_per_image.softmax(dim=1)
+print(probs)
