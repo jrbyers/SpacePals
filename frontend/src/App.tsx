a2e35736aab43bb3deaff6da36c85818855d80ad
MarcelMatsal <marcel_mateos_salles@brown.edu>
2024-02-03 13:34:45 -0500

    brought in frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { ContextProvider } from "./components/input/ContextProvider";
+
+import SongsPage from "./pages/input/SongsPage";
+import GenerationPage from "./pages/input/GenerationPage";
+import GeneratedPlaylistPage from "./pages/result/GeneratedPlaylistPage";
+import AboutPage from "./pages/home/AboutPage";
+import FAQPage from "./pages/home/FAQPage";
+
+import "./App.css";
+import SettingsPage from "./pages/input/SettingsPage";
+import Toolbar from "./components/toolbar/Toolbar";
+import LeaderBoardPage from "./pages/home/LeaderBoardPage";
+import HomePage from "./pages/home/HomePage";
+
+/**
+ * This is the highest level component!
+ */
+function App() {
+  return (
+    <ContextProvider>
+      <div className="App">
+        <Toolbar />
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/faq" element={<FAQPage />} />
+            <Route path="/leaderboard" element={<LeaderBoardPage />} />
+            <Route path="/input/songs" element={<SongsPage />} />
+            <Route path="/input/settings" element={<SettingsPage />} />
+            <Route path="/input/lyrics" element={<GenerationPage />} />
+            <Route path="/result" element={<GeneratedPlaylistPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </ContextProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/toolbar/Toolbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Toolbar.css"
+
+//Main component of the tool bar
+function Toolbar() {
+
+    //returns the component
+    return (
+    <header className="header">
+        <a href="/" className="logo">SpotiDuo</a>
+
+            <nav className="navbar">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/faq">FAQ</a>
+                <a href="/leaderboard">Leaderboard</a>
+            </nav>
+    </header>
+    )
+}
+
+export default Toolbar;
